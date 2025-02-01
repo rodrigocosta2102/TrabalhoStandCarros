1232dc5fe5c7b15620e93d0abdfebe67e5d219cc
rodrigocosta2102 <a14646@esars.pt>
2025-02-01 21:37:21 +0000

    Criei a lista de cores e verifiquei se o campo da cor introduzida estava preenchido e se a cor era válida
    Também ajustei alguns erros

--- carro.py
+++ carro.py
@@ -1,6 +1,7 @@
 # Classe para Carro
 class Carro:
    lista_marcas = ["Mercedes", "BMW", "Bugatti", "Porsche", "Ferrari", "Lexus"]
+   lista_cores = ["Preto", "Azul", "Vermelho", "Laranja", "Cinzento", "Branco", "Verde"]
   
   def __init__(self, marca, modelo, cor, ano, preco, potencia, disponibilidade, matricula):
         self.marca = marca
@@ -12,22 +13,26 @@ class Carro:
         self.disponibilidade = disponibilidade
         self.matricula = matricula
 
-    def vender(self):
-        self.disponibilidade = False
+   def vender(self):
+      self.disponibilidade = False
 
-    def __str__(self):
-        return f"{self.marca}; {self.modelo}; {self.cor}; {self.ano}; {self.preco}; {self.potencia}; {self.disponibilidade}; {self.matricula}"
+   def __str__(self):
+      return f"{self.marca}; {self.modelo}; {self.cor}; {self.ano}; {self.preco}; {self.potencia}; {self.disponibilidade}; {self.matricula}"
 
-    @staticmethod
-    def from_string(data_str):
-        marca, modelo, cor, ano, preco, potencia, disponibilidade, matricula = data_str.strip().split(";")
-        return Carro(marca, modelo, cor, int(ano), float(preco), int(potencia), disponibilidade == "True", matricula)
+   @staticmethod
+   def from_string(data_str):
+      marca, modelo, cor, ano, preco, potencia, disponibilidade, matricula = data_str.strip().split(";")
+      return Carro(marca, modelo, cor, int(ano), float(preco), int(potencia), disponibilidade == "True", matricula)
 
-    def is_valid(self):
-        if not self.marca or not self.modelo:
-            print("Erro: O carro deve ter uma marca e um modelo.")
-            return False
+   def is_valid(self):
+      if not self.marca or not self.modelo:
+         print("Erro: O carro deve ter uma marca e um modelo.")
+         return False
 
-        if self.marca not in Carro.lista_marcas:
-            print(f"Erro: A marca '{self.marca}' não é válida. Marcas permitidas: {', '.join(Carro.lista_marcas)}.")
+      if self.marca not in Carro.lista_marcas:
+         print(f"Erro: A marca '{self.marca}' não é válida. Marcas permitidas: {', '.join(Carro.lista_marcas)}.")
+         return False
+
+      if not self.cor or self.cor not in Carro.lista_cores:
+            print(f"Erro: A cor '{self.cor}' não é válida. Cores permitidas: {', '.join(Carro.lista_cores)}.")
             return False
