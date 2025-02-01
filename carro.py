90bb77b7483f8c7cb5241aefe392cb2876a43abe
rodrigocosta2102 <a14646@esars.pt>
2025-02-01 21:19:27 +0000

    Criei a class Carro

--- carro.py
+++ carro.py
@@ -1,1 +1,15 @@
+# Classe para Carro
+class Carro:
 
+  def __init__(self, marca, modelo, cor, ano, preco, potencia, disponibilidade, matricula):
+        self.marca = marca
+        self.modelo = modelo
+        self.cor = cor
+        self.ano = ano
+        self.preco = preco
+        self.potencia = potencia
+        self.disponibilidade = disponibilidade
+        self.matricula = matricula
+
+    def vender(self):
+        self.disponibilidade = False
