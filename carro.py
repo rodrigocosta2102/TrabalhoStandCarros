f07549138d0311f8cfc3f66a7988981c180ef847
rodrigocosta2102 <a14646@esars.pt>
2025-02-01 21:24:45 +0000

    Criei uma string e uma função from_string
    A string serve para definir como os dados do Carro vão ser exibidos e a função from_string serve para criar um objeto através dessa string.

--- carro.py
+++ carro.py
@@ -13,3 +13,11 @@ class Carro:
 
     def vender(self):
         self.disponibilidade = False
+
+    def __str__(self):
+        return f"{self.marca}; {self.modelo}; {self.cor}; {self.ano}; {self.preco}; {self.potencia}; {self.disponibilidade}; {self.matricula}"
+
+    @staticmethod
+    def from_string(data_str):
+        marca, modelo, cor, ano, preco, potencia, disponibilidade, matricula = data_str.strip().split(";")
+        return Carro(marca, modelo, cor, int(ano), float(preco), int(potencia), disponibilidade == "True", matricula)
