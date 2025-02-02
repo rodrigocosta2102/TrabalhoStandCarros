858a981ea3313b8b69f021ada8b1c7f0a88bf4b9
rodrigocosta2102 <a14646@esars.pt>
2025-02-02 22:51:59 +0000

    Criei a função carregar_carros

--- persistencia.py
+++ persistencia.py
@@ -10,3 +10,15 @@ def gravar_carros(ficheiro, lista_carro):
         for carro in lista_carro:
             f.write(str(carro) + "\n")
     print(f"Carros gravados em '{ficheiro}' com sucesso!")
+
+def carregar_carros(ficheiro):
+    carros = []
+    try:
+        with open(ficheiro, "r") as f:
+            for linha in f:
+                carro = Carro.from_string(linha)
+                carros.append(carro)
+        print(f"Carros carregados de {ficheiro} com sucesso!")
+    except FileNotFoundError:
+        print(f"O ficheiro {ficheiro} não foi encontrado.")
+    return carros
