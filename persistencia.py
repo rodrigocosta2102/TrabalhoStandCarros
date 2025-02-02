eb90bb779cd15d49122bf275dfd0280b40d8a475
rodrigocosta2102 <a14646@esars.pt>
2025-02-02 22:49:53 +0000

    Criei a função gravar_carros

--- persistencia.py
+++ persistencia.py
@@ -1,2 +1,12 @@
 # persistencia.py
 from carro import Carro
+
+def gravar_carros(ficheiro, lista_carro):
+    if not lista_carro:
+        print("A lista de carros está vazia.")
+        return
+
+    with open(ficheiro, "w") as f:
+        for carro in lista_carro:
+            f.write(str(carro) + "\n")
+    print(f"Carros gravados em '{ficheiro}' com sucesso!")
